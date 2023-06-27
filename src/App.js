fbc470354520ed47002d790001d607980a89ff82
thesagarmahajan <sagarmahajan31997@gmail.com>
2023-06-27 12:25:27 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Change for commit 2
         </p>
         <a
           className="App-link"
